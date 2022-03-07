917b1058a2707e1fe91f7e47d3bd4cc49cf4ad34
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-03-07 17:30:13 +0300

    feat: add getCompanies for ServerApi.js

--- static/js/ServerApi.js
+++ static/js/ServerApi.js
@@ -5,11 +5,32 @@ export default class ServerApi {
 
     static skills = `${ServerApi.baseApiURL}/skills`;
 
+    static companies = `${ServerApi.baseApiURL}/companies`;
+
     static vacancyList = `${ServerApi.baseURL}/vacancy_list`;
 
     static async getSkills() {
         const response = await fetch(
             `${ServerApi.skills}`,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+            }
+        );
+        if (!response.ok) {
+            console.log(
+                `Sorry, but there is ${response.status} error: ${response.statusText}`
+            );
+            return [];
+        }
+        return response.json();
+    }
+
+    static async getCompanies() {
+        const response = await fetch(
+            `${ServerApi.companies}`,
             {
                 method: 'GET',
                 headers: {
