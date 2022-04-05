9e92dcef0395517198739240b06fde1e27a56e5c
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-04-05 14:36:35 +0300

    feat: add getPbhCompanies(), del companiesVisa.js

--- static/js/ServerApi.js
+++ static/js/ServerApi.js
@@ -3,6 +3,8 @@ export default class ServerApi {
 
     static companies = `/api/companies`;
 
+    static pbhCompanies = `/api/pbh_companies`;
+
     static vacancyList = `/api/vacancy_list`;
 
     static async getSkills() {
@@ -27,6 +29,25 @@ export default class ServerApi {
     static async getCompanies() {
         const response = await fetch(
             `${ServerApi.companies}`,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+            }
+        );
+        if (!response.ok) {
+            console.log(
+                `Sorry, but there is ${response.status} error: ${response.statusText}`
+            );
+            return [];
+        }
+        return response.json();
+    }
+
+    static async getPbhCompanies() {
+        const response = await fetch(
+            `${ServerApi.pbhCompanies}`,
             {
                 method: 'GET',
                 headers: {
